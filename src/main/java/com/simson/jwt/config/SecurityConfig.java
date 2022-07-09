b3bce4f1b489a5ca32ebeb71acb0ed6882904c95
qwe5507 <apvmffkdls@gmail.com>
2022-07-09 23:48:08 +0900

    feat :  jwt를 위한 security 설정

---
+++ src/main/java/com/simson/jwt/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.simson.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //세션을 사용하지 않겠다, Stateless하게 만들겠다.
+                .and()
+                .addFilter(corsFilter)
+                .formLogin().disable() //jwt를 사용하기 때문에 id, password를 입력하여 접속하지 않는다.
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll(); // 위의 api 외의 요청들은 권한 없이 접근 가능!.
+
+    }
+}
